ed8da9bda2cc98aae08f0c1e190ffa46c6556c95
Kourchenko <dkourchenko@gmail.com>
2023-07-15 13:00:07 -0700

    Mobile friendly LandingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,23 +8,24 @@ const LandingPage = ({ onLogin }) => {
 
     return (
         <div style={{ padding: '50px' }}>
-            <Row justify="center" align="middle" style={{ height: '100vh' }}>
-                <Col span={8}>
+            <Row justify="center" align="middle" style={{ height: '80vh' }}>
+                <Col span={24}>
                     <div style={{ textAlign: 'center' }}>
                         <Title level={2}>Welcome to GitHub Contributions</Title>
                         <Title level={3}>A place to view your contributions.</Title>
-                        <Divider />
-                        <>
-                            <Button
-                                type="primary"
-                                icon={<GoogleOutlined />}
-                                size="large"
-                                block
-                                onClick={() => onLogin()}
-                            >
-                                Sign in with Google
-                            </Button>
-                        </>
+                    </div>
+                </Col>
+                <Col span={12}>
+                    <div style={{ textAlign: 'center' }}>
+                        <Button
+                            type="primary"
+                            icon={<GoogleOutlined />}
+                            size="large"
+                            block
+                            onClick={() => onLogin()}
+                        >
+                            Sign in with Google
+                        </Button>
                     </div>
                 </Col>
             </Row>
